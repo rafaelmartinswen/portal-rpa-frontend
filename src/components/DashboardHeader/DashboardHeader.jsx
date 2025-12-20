1239a4bdb51e2df275b7bca8de6db4e667978785
Vitor Lamoya <lamoyav@gmail.com>
2025-12-20 16:46:56 -0300

    feat: Adicionando tela de Project Management

--- src/components/DashboardHeader/DashboardHeader.jsx
+++ src/components/DashboardHeader/DashboardHeader.jsx
@@ -48,6 +48,8 @@ function DashboardHeader({ user }) {
         return <Scheduler />;
       case "management":
         return <Management />;
+      case "projectManagement":
+        return <ProjectManagement />;
       default:
         return <SpecificProject project={activeTab} />;
     }
