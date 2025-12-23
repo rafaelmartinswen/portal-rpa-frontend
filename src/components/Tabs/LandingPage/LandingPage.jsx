63cf913ca896a4f61163fc88fd9fa409bc55c7cd
Vitor Lamoya <lamoyav@gmail.com>
2025-12-23 12:20:32 -0300

    fix: Ajsutes

--- src/components/Tabs/LandingPage/LandingPage.jsx
+++ src/components/Tabs/LandingPage/LandingPage.jsx
@@ -12,11 +12,6 @@ function LandingPage({ user, onTabChange }) {
     const [alerts, setAlerts] = useState([]);
     const [openAlert, setOpenAlert] = useState(false);
     const [selectedPage, setSelectedPage] = useState(null);
-    useEffect(() => {
-        updateRobotsList();
-        updateAlerts();
-      }, [updateRobotsList, updateAlerts]);
-
     const updateRobotsList = useCallback(async () => {
         try {
             const res = await fetch(`${API_URL}/robots`);
@@ -36,6 +31,11 @@ function LandingPage({ user, onTabChange }) {
             console.error("Erro ao buscar alertas:", error);
         }
     }, []);
+
+    useEffect(() => {
+        updateRobotsList();
+        updateAlerts();
+      }, [updateRobotsList, updateAlerts]);
 
     function handleAction(page) {
         setSelectedPage(page);
