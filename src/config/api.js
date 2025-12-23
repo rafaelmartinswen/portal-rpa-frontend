61f218df983176211055c0008e895e2d66c5449a
Vitor Lamoya <lamoyav@gmail.com>
2025-12-23 18:12:26 -0300

    fix: Funciona

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  "https://portal-rpa-backend.bravedune-0c4b692e.eastus2.azurecontainerapps.io";
+  "https://portal-rpa-backend-dev.lemonbay-67299622.brazilsouth.azurecontainerapps.io";
